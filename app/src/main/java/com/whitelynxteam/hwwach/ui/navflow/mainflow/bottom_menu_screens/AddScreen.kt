53dd7e2e653a128bdb04859f99db7c351d3d5887
Yuris <yuris.musin@mail.ru>
2026-01-05 19:40:15 +0500

    добавил навигацию в bottom menu

---
+++ app/src/main/java/com/whitelynxteam/hwwach/ui/navflow/mainflow/bottom_menu_screens/AddScreen.kt
@@ -0,0 +1,28 @@
+package com.whitelynxteam.hwwach.ui.navflow.mainflow.bottom_menu_screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+
+@Composable
+fun AddScreen(
+    modifier: Modifier = Modifier
+) {
+
+    Box(
+        modifier = modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "ДОБАВИТЬ",
+            textAlign = TextAlign.Center
+        )
+    }
+}
